d99d759a07e01a3695179092ffc9fabe7faf5730
Pema Wangchuk <70899207+pemawangchuk@users.noreply.github.com>
2026-05-21 19:42:12 +0800

    Update celery.py

--- tickets/celery.py
+++ tickets/celery.py
@@ -1,1 +1,19 @@
 
+import os
+
+from celery import Celery
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'eticket_project.settings'
+)
+
+app = Celery('eticket_project')
+
+app.config_from_object(
+    'django.conf:settings',
+    namespace='CELERY'
+)
+
+app.autodiscover_tasks()
+
