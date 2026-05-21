3cdfe4217c3d8abaf8a2d423b96c45b1f0f306b3
Pema Wangchuk <70899207+pemawangchuk@users.noreply.github.com>
2026-05-21 19:41:18 +0800

    Update kafka_producer.py
    change

--- tickets/utils/kafka_producer.py
+++ tickets/utils/kafka_producer.py
@@ -1,1 +1,25 @@
 
+from kafka import KafkaProducer
+
+import json
+
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+
+def send_ticket_created(ticket):
+
+    data = {
+        "ticket_id": str(ticket.ticket_id),
+        "ticket_number": ticket.ticket_number
+    }
+
+    producer.send(
+        'ticket_created',
+        data
+    )
+
+    producer.flush()
